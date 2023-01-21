2efafa430de51461ee434df39020ac60dadf82f0
Hasibur Rahman <59811661+hasibkyau@users.noreply.github.com>
2023-01-21 15:48:50 +0600

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 //to install Axios run: npm install axios
 
 import axios from 'axios';    
-axios.get("https://dht-11-7fe63-default-rtdb.firebaseio.com")
+axios.get("https://dht-11-7fe63-default-rtdb.firebaseio.com/Location.json")
         .then(response => {
             conslol.log(response.data);
             localStorage.setItem("Data", JSON.stringify(response.data))
